38c546de99bdc04cf6f314132041ef71edde7607
Lynn <zzzzzzzl@tamu.edu>
2025-12-04 16:04:19 -0600

    lc

--- src/leetcode/editor/en/LC117_PopulatingNextRightPointersInEachNodeIi.java
+++ src/leetcode/editor/en/LC117_PopulatingNextRightPointersInEachNodeIi.java
@@ -1,34 +1,34 @@
 package src.leetcode.editor.en;
 
-import src.leetcode.editor.en.utils2.Node;
 
 import java.util.ArrayDeque;
 import java.util.Queue;
 
 public class LC117_PopulatingNextRightPointersInEachNodeIi {
     //leetcode submit region begin(Prohibit modification and deletion)
-/*
 // Definition for a Node.
-class Node {
-    public int val;
-    public Node left;
-    public Node right;
-    public Node next;
+    class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
 
-    public Node() {}
-    
-    public Node(int _val) {
-        val = _val;
+        public Node() {
+        }
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, Node _left, Node _right, Node _next) {
+            val = _val;
+            left = _left;
+            right = _right;
+            next = _next;
+        }
     }
 
-    public Node(int _val, Node _left, Node _right, Node _next) {
-        val = _val;
-        left = _left;
-        right = _right;
-        next = _next;
-    }
-};
-*/
+    ;
 
     class Solution {
         public Node connect(Node root) {
